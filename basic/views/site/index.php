e0c6c7eeeb4b96c7905a1d23c70fede5e1f2b112
14bd02002 <dias8888d@gmail.com>
2018-02-20 15:27:06 +0600

    creating project

---
+++ basic/views/site/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'База всех организаций Казахстана';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Добро Пожаловать на сайт alldata.kz</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        
+    </div>
+</div>
